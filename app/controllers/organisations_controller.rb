75d065926f186160d6a62631f60e8c29de830e50
Akira Hora <horzta@gmail.com>
2019-03-26 08:16:15 +0800

    What happened?

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,2 +1,14 @@
 class OrganisationsController < ApplicationController
+    def organisation_params
+    	params.require(:user).permit(:name, :hour_rate)
+    end
+
+    def create
+    	@organisation = Organisation.new(params[:organisation])
+    	if @organisation.save
+    		redirect to @organisation
+    	else
+    		render "new"
+    	end	
+    end
 end
--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,2 +1,10 @@
 class ShiftsController < ApplicationController
+    def shift_params
+    	params.require(:user).permit(:shift_start, :shift_finish, :break_length)
+    end
+
+    def create
+    	
+    end
+
 end
